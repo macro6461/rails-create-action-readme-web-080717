ef03265d8d66afdbd55e39dd73fedfcf8be9ccca
macro6461 <mattcroak718@gmail.com>
2017-09-05 08:03:34 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,15 @@ class PostsController < ApplicationController
 		@posts = Post.all
 	end
 
+	def new
+	end
+
+	def create
+		@post = Post.create(title: params[:title], description: params[:description])
+		redirect_to post_path(@post) #pass instance variable as argument for posts_path
+		#this brings us to /posts/:id -- the show page for this new post
+	end
+
 	def show
 		@post = Post.find(params[:id])
 	end
